882ae2bbf3b21c57a37b7fe150ba3a02b62b8b00
clmcdonald9 <clmcdonald9@alaska.edu>
2026-03-23 12:20:34 -0800

    collatzcount passes tests

--- assignments/pa5/PA5.hs
+++ assignments/pa5/PA5.hs
@@ -3,6 +3,7 @@
 -- Module for CS 331 programming assignment 5
 
 -- start date: 3/19/26
+-- last updated: 3/21/26
 
 -- PA5.hs was written using Glenn G. Chappells PA5.hs skeleton as a base
 
@@ -15,12 +16,12 @@ module PA5 where
 
 -- collatzCounts
 collatzCounts :: [Integer]
-collatzCounts 
+collatzCounts = map collatzSteps [1..]
 
-collatz n
+collatzSteps n
   | n == 1 = 0
-  | even n = 1 + collatzCounts(div n 2)
-  |otherwise = 1 + collatzCounts(3 * n + 1)
+  | even n = 1 + collatzSteps(div n 2)
+  |otherwise = 1 + collatzSteps(3 * n + 1)
     
 
 -- =====================================================================
@@ -28,8 +29,11 @@ collatz n
 
 -- operator #<
 (#<) :: Ord a => [a] -> [a] -> Int
-_ #< _ = 42  -- DUMMY; REWRITE THIS!!!
-
+[] #< _ = 0
+_ #< [] = 0
+(a:as) #< (b:bs)
+  | a < b = 1 + (as #< bs)
+  | otherwise = (as #< bs)
 
 -- =====================================================================
 
